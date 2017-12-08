32522eb4aabbc847a33553c9cea9b9ad9621d864
noah-ryan31 <noahryan1998@gmail.com>
2017-12-08 15:33:06 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function changePicture(elem){
+	document.getElementById("f1").classList.toggle("flag4");
+	document.getElementById("f1").classList.toggle("green0");
+}
+function changePicture1(elem){
+	document.getElementById("f2").classList.toggle("flag1");
+	document.getElementById("f2").classList.toggle("green1");
+}
+function changePicture2(elem){
+	document.getElementById("f3").classList.toggle("flag2");
+	document.getElementById("f3").classList.toggle("green2");
+}
+function changePicture3(elem){
+	document.getElementById("f4").classList.toggle("flag3");
+	document.getElementById("f4").classList.toggle("green3");
+}
+function changePicture4(elem){
+	document.getElementById("f5").classList.toggle("stars");
+	document.getElementById("f5").classList.toggle("green4");
+}
+function changePicture5(elem){
+	document.getElementById("f6").classList.toggle("flag5");
+	document.getElementById("f6").classList.toggle("green5");
+}
+function changePicture6(elem){
+	document.getElementById("f7").classList.toggle("flag6");
+	document.getElementById("f7").classList.toggle("green6");
+}
+function changePicture7(elem){
+	document.getElementById("f8").classList.toggle("flag7");
+	document.getElementById("f8").classList.toggle("green7");
+}
+function changePicture8(elem){
+	document.getElementById("f9").classList.toggle("flag8");
+	document.getElementById("f9").classList.toggle("green8");
+}
